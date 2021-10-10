3eac11a637bae47e9e79f5f8f469614be7c3ce6f
Marianchuk <marianchuksv@gmail.com>
2021-10-10 18:34:28 +0300

    css

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
+/*
+function debounce (func, wait = 20, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function(){
+            timeout = null;
+            if (!immediate) func.apply(context, agg);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout - setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 
+
+
+
+const sliderImages = document.querySelectorAll('.trainers');
+
+function checkSlide(e) {
+    console.log(window.scrollY);
+    sliderImages.forEach(slideImage => {
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImages.height / 2;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImages.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+         } else {
+             sliderImage.classList.remove('active');
+         }
+        
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+
+*/
