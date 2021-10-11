273b8dcf4a0d5ec73ec56e4f8e38b1d39f983d9f
Marianchuk <marianchuksv@gmail.com>
2021-10-11 22:25:57 +0300

    js

--- js/program.js
+++ js/program.js
@@ -1,15 +1,28 @@
 
 const tabLinks = document.querySelectorAll('.tab');
-
+const tabsItems = document.querySelectorAll('.tabcontent');
 
 tabLinks.forEach(function(item) {
     item.addEventListener('click', function() {
         let currentTab = item;
+        let tabId = currentTab.getAttribute('data-tab');
+        let currentBtn = document.querySelector(tabId);
 
-        tabLinks.forEach(function(item) {
+        if ( ! currentTab.classList.contains('active')) {
+            tabLinks.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            tabsItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            currentTab.classList.add('active');
+            currentBtn.classList.add('active');
+        }
 
-        currentTab.classList.add('active');
+        
     });
 
 });
-});
+
